dca98f06a3b655690fd95d48bc68ca1fdf51be2a
garaiza-93 <araizagustavo93@gmail.com>
2022-10-18 18:57:24 -0500

    filter out NvimTree in bufferline

--- lua/config/bufferline-config/init.lua
+++ lua/config/bufferline-config/init.lua
@@ -1,6 +1,18 @@
 require('bufferline').setup {
   options = {
     mode = 'tabs',
+    always_show_bufferline = true,
+    separator_style = 'thin',
+    custom_filter = function(buf_number, _)
+      if 
+        vim.bo[buf_number].filetype ~= 'startify' and
+        vim.bo[buf_number].filetype ~= 'NvimTree'
+      then
+        return true
+      else
+        return false
+      end
+    end,
     show_close_icon = false,
     hover = {
       enabled = true,
