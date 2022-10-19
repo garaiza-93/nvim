6d47d59a10bda974107fc41249abc9fd1c96dfdd
garaiza-93 <araizagustavo93@gmail.com>
2022-10-19 15:49:04 -0500

    implement mason, add mason filetype to blankline config

--- lua/config/indent-blankline-config/init.lua
+++ lua/config/indent-blankline-config/init.lua
@@ -3,5 +3,5 @@ require('indent_blankline').setup {
   show_end_of_line = false,
   disable_with_no_list = true,
   buftype_exclude = { 'terminal' },
-  filetype_exclude = { 'help', 'git', 'markdown', 'snippets', 'text', 'startify'},
+  filetype_exclude = { 'help', 'git', 'markdown', 'snippets', 'text', 'startify', 'mason'},
 }
--- lua/config/lsp-cmp-config/init.lua
+++ lua/config/lsp-cmp-config/init.lua
@@ -1,3 +1,4 @@
 require('cmp_setup')
 require('filetype_configs')
+require('mason').setup()
 require('language_servers')
