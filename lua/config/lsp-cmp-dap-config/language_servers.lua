c976fb01560bfa256e37d3d9a5b97d1cb063c592
garaiza-93 <araizagustavo93@gmail.com>
2022-10-21 00:47:09 -0500

    fix cmp configuration only working in it's own directory

--- lua/config/lsp-cmp-dap-config/language_servers.lua
+++
@@ -1,6 +0,0 @@
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['html'].setup {
-  capabilities = capabilities
-}
