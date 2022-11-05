11096a13398662fb749723b9784e62ce5713dc3a
garaiza-93 <araizagustavo93@gmail.com>
2022-11-04 20:54:26 -0500

    implement intelliJ-like mapping for code completion

--- lua/config/lsp-cmp-dap-config/init.lua
+++ lua/config/lsp-cmp-dap-config/init.lua
@@ -1,10 +1,4 @@
 -- Set up nvim-cmp.
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~=0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-end
-
-local luasnip = require('luasnip')
 local cmp = require('cmp')
 
 vim.g.completeopt = 'menu, menuone, noselect, noinsert'
@@ -25,28 +19,18 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
+        local entry = cmp.get_selected_entry()
+        if not entry then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          cmp.confirm()
+        end
       else
         fallback()
       end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }), 
+    end, {"i","s","c",}),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
