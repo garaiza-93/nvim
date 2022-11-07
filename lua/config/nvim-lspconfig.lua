3e8530957df4e654202da35858544a65eebe33eb
garaiza-93 <araizagustavo93@gmail.com>
2022-11-06 19:16:14 -0600

    fix warning about vim being an undeclared global

--- lua/config/nvim-lspconfig.lua
+++ lua/config/nvim-lspconfig.lua
@@ -5,5 +5,24 @@ require('mason-lspconfig').setup_handlers {
     require('lspconfig')[server_name].setup {}
   end,
   -- Dedicated handlers for specific servers after this line
-  -- ['server'] = function ()
+  ['sumneko_lua'] = function()
+    require('lspconfig').sumneko_lua.setup {
+      settings = {
+        Lua = {
+          runtime = {
+            version = 'LuaJIT',
+          },
+          diagnostics = {
+            globals = { 'vim' },
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          telemety = {
+            enable = false,
+          },
+        },
+      },
+    }
+  end
 }
