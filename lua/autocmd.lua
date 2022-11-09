f1610a796a1c6ab0afb4d55f40941ad3f2c125a5
garaiza-93 <araizagustavo93@gmail.com>
2022-11-08 20:46:10 -0600

    cooler alpha header and autocmd for cosmetic stuff

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -3,7 +3,6 @@ local new_cmd = vim.api.nvim_create_autocmd
 
 --Groups
 local packerGrp = new_group('packer', { clear = true })
-local ts_workaroundGrp = new_group('treesitter', { clear = true })
 local lspGrp = new_group('lsp', { clear = true })
 --Format file before write
 new_cmd(
@@ -30,5 +29,31 @@ new_cmd(
       vim.cmd { cmd = 'PackerSync' }
     end,
     group = packerGrp
+  }
+)
+-- Hide status/tab/cmd lines in alpha greeter
+new_cmd(
+  { 'User' },
+  {
+    pattern = 'AlphaReady',
+    desc = 'disable status, tabline, and cmdline',
+    callback = function()
+      vim.go.laststatus = 0
+      vim.opt.showtabline = 0
+      vim.opt.cmdheight = 0
+    end,
+  }
+)
+-- And to reenable it
+new_cmd(
+  { 'BufUnload' },
+  {
+    buffer = 0,
+    desc = 'enable what we disabled',
+    callback = function()
+      vim.go.laststatus = 2
+      vim.opt.showtabline = 2
+      vim.opt.cmdheight = 1
+    end,
   }
 )
