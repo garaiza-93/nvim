857b33d4a49df1de9f5629d13814b278c75bdf75
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2022-11-11 17:41:46 -0600

    remove unused info from statusline, add info on buffer list

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -2,7 +2,24 @@ require('lualine').setup {
   options = {
     theme = 'auto',
     globalstatus = true,
-    disabled_filetypes = { 'startify' },
+    disabled_filetypes = { 'alpha' },
     ignore_focus = {},
   },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff' },
+    lualine_c = {
+      {
+        'buffers',
+        hide_filename_extension = true,
+        symbols = {
+          alternate_file = '',
+        },
+        mode = 4,
+      },
+    },
+    lualine_x = { 'filetype' },
+    lualine_y = { 'diagnostics' },
+    lualine_z = { 'location' }
+  }
 }
