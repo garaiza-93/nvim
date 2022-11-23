8c428bb4605a4c74ca26a44ee9d55d29b149899c
garaiza-93 <araizagustavo93@gmail.com>
2022-11-23 12:20:49 -0600

    source nvim-related items before plugins

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 -- Imports
 require('impatient')
-require('plugins')
-require('mason').setup()
 require('autocmd')
 -- require('./config/treesitter-config')
 -- require('./config/tokyonight-config')
 require('./settings/keybinds')
 require('./settings/globals')
 require('./settings/options')
+require('plugins')
+require('mason').setup()
