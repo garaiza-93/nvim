b2eb571b03b8383fe3f0292ef1117d2c53aa8866
garaiza-93 <araizagustavo93@gmail.com>
2023-01-16 22:31:19 -0600

    bootstrap lazy

--- init.lua
+++ init.lua
@@ -1,10 +1,19 @@
+-- Bootstrap lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 -- Imports
-require('impatient')
 require('autocmd')
--- require('./config/treesitter-config')
--- require('./config/tokyonight-config')
 require('./settings/keybinds')
 require('./settings/globals')
 require('./settings/options')
-require('plugins')
-require('mason').setup()
+require('lazy').setup("plugins")
